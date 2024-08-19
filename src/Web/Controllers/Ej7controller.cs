d18adf4cba8d551f9c6bb2a4d93bf6fb93b26750
Franco <cfranco3102@gmail.com>
2024-08-19 16:16:00 -0300

    Ejercicio 7 resuelto

---
+++ src/Web/Controllers/Ej7controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej7controller : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult Get(int start, int end)
+    {
+        int difference = end - start;
+        List<int> numbers = new List<int>();
+        if (difference < 1000)
+        {
+            for (int i = start; i < difference; i++)
+            {
+                numbers.Add(i);
+            }
+            return Ok(numbers);
+        } else
+        {
+            return BadRequest("La diferencia entre el número de fin y el de inicio es mayor a 1000");
+        }
+
+    }
+}
