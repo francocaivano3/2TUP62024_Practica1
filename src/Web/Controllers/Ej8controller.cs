0fec23253fee2ed8c14a198549590cbdad245aba
Franco <cfranco3102@gmail.com>
2024-08-19 16:26:25 -0300

    Ejercicio 8 resuelto

---
+++ src/Web/Controllers/Ej8controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej8controller : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult Get(int start, int end)
+    {
+        int difference = end - start;
+        List<int> numbers = new List<int>();
+        int i = start;
+        while(i <= end && difference < 1000) 
+        {
+            numbers.Add(i);
+            i++;
+        }
+        if(numbers.Count > 0)
+        {
+            return Ok(numbers);
+        } else
+        {
+            return BadRequest("La diferencia entre los dos números es mayor o igual a 1000");
+        }
+    }
+}
