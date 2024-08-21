e6ef021d78c9407f476b1db30b98479a2fa4e245
Franco <cfranco3102@gmail.com>
2024-08-21 17:45:47 -0300

    Agrego ejercicio 7 de LINQ

---
+++ src/Web/Controllers/LinqEj7controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj7controller : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult Get([FromQuery]string text)
+    {
+        string[] upperList = text.Split(" ");
+        
+        var result = upperList.Where(x => x.ToUpper() == x).ToList();
+
+        return Ok(result);
+    }
+}
