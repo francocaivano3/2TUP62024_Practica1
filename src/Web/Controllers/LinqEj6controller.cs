3fbed5f2e0cee001d18b1564371fd7c3f3fe9adc
Franco <cfranco3102@gmail.com>
2024-08-21 15:51:35 -0300

    Agrego ejercicio 6 de LINQ

---
+++ src/Web/Controllers/LinqEj6controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj6controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<string> words)
+    {
+        string toReplace = "ia";
+        var result = words.Where(x => x.Length > 0).Select(x => x.Replace(toReplace, "*")).ToList();
+        return Ok(result);
+    }
+}
