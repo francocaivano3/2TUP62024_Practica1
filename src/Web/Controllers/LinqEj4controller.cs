ebead806653ef2415c1b17e761e73651ff0a2696
Franco <cfranco3102@gmail.com>
2024-08-21 14:39:50 -0300

    Agrego ejercicio 4 de LINQ

---
+++ src/Web/Controllers/LinqEj4controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj4controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<int> numbers)
+    {
+        int position = 0;
+        var result =
+           from number in numbers
+           where position < 5
+           select numbers[position];
+        position++;
+
+        return Ok(result.ToList());
+    }
+}
