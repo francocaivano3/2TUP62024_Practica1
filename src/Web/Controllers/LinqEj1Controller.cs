8c60b5544fbe3f9568d8535fd7cd69a99012dd08
Franco <cfranco3102@gmail.com>
2024-08-21 13:48:07 -0300

    Agrego ejercicio 1 de LINQ

---
+++ src/Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj1Controller : ControllerBase
+{
+    [HttpGet]   
+    public IActionResult Get([FromQuery] List<int> numbers, int min, int max)
+    {
+        var result = 
+            from num in numbers
+            where num >= min && num <= max
+            select num;
+        
+        return Ok(result.ToList());
+    }
+}
