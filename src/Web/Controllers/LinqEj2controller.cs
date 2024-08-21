6d8b20d2807bc2f04ad3e86bbe87433475eb6997
Franco <cfranco3102@gmail.com>
2024-08-21 14:01:14 -0300

    Agrego ejercicio 2 de LINQ

---
+++ src/Web/Controllers/LinqEj2controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj2Controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<string> words)
+    {
+        var result =
+            from word in words
+            where word.Length >= 5
+            select word.ToUpper();
+
+
+        return Ok(result.ToList());
+    }
+}
