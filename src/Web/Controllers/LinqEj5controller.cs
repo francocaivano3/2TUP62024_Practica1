092cd1233a4e10fa8313832ffb36b071e2e02255
Franco <cfranco3102@gmail.com>
2024-08-21 15:04:57 -0300

    Agrego ejercicio 5 LINQ

---
+++ src/Web/Controllers/LinqEj5controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj5controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<int> numbers)
+    {
+        var result =
+           from number in numbers
+           where number * number > 20
+           select number;
+
+        List<string> originalSquares = new List<string>();
+        foreach (var number in result.ToList())
+        {
+            string strNumb = number.ToString();
+            int square = number * number;
+            string strSquare = square.ToString();
+            originalSquares.Add($"{strNumb} - {strSquare}");
+        }
+        return Ok(result);
+    }
+}
