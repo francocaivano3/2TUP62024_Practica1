5e63596c0a5224976b85aee3950a0707e7a7e431
Franco <cfranco3102@gmail.com>
2024-08-21 14:07:03 -0300

    Agrego ejercicio 3 de LINQ

---
+++ src/Web/Controllers/LinqEj3controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj3Controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<string> words)
+    {
+        var result =
+           from word in words
+           where word[0] == 'b' && word[word.Length - 1] == 'r'
+           select word;
+
+
+        return Ok(result.ToList());
+    }
+}
